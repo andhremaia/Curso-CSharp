dd00cc1d3303f87b905a866f4a399a11af638a6f
Andre Maia <andhremaia@gmail.com>
2024-01-19 18:32:51 -0300

    Resolução dos exercicios 1, 2 e 3 de Estrutura for

---
+++ 3 - Recapitulação de Lógica de Programação usando/Exercicios/Estrutura For/exercicio1.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero maior que 0 e menor ou igual a 1000: ");
+            int x = int.Parse(Console.ReadLine()!);
+
+            if (x < 0 || x > 1000) 
+            {
+                Console.WriteLine("Fora do intervalo");
+            }
+            else 
+            {                
+                for (int i = 1; i <= x; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 3 - Recapitulação de Lógica de Programação usando/Exercicios/Estrutura For/exercicio2.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de numeros: ");
+            int n = int.Parse(Console.ReadLine()!);
+            int x;
+            int dentro = 0;
+            int fora = 0;
+
+            for (int i = 1;  i <= n; i++) {
+                x = int.Parse(Console.ReadLine()!);
+                if (x >= 10 && x <= 20)
+                    dentro += 1;
+                else
+                    fora += 1;
+            }
+            Console.WriteLine("{0} in \n{1} out", dentro, fora);
+        }
+    }
+}
